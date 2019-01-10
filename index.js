166261f06090bcae9dcc471e20884553c6e0758c
Ben Manekin <ben.manekin@gmail.com>
2019-01-10 23:45:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(0, name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.shift()
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
